92eebe5ebf41a6487991c583b4ffc6e0fdf60f25
mjschrader307 <mjschrader307@gmail.com>
2025-05-01 20:23:02 -0600

    Done

--- code.test.js
+++ code.test.js
@@ -1,6 +1,14 @@
-const fs = require('fs');
-const assert = require('assert');
-eval(fs.readFileSync('code.js')+'');
+const fs = require("fs");
+const assert = require("assert");
+eval(fs.readFileSync("code.js") + "");
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+
+// Same thing, using new functions
+assert(factorial_alt(10) === 3628800);
+assert(e_alt(10) === 2.7182818011463845);
+
+// Base case
+assert(factorial_alt(0) === 1);
+assert(e_alt(0) === 1);
